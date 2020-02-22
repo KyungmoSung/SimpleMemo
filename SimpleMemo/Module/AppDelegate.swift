6d6306bb1a4e303b5eb5dc51acaea3c78e17b8c1
Sung Kyungmo <skm280@naver.com>
2020-02-23 04:02:06 +0900

    네비게이션 뒤로가기 버튼 커스텀

--- SimpleMemo/Module/AppDelegate.swift
+++ SimpleMemo/Module/AppDelegate.swift
@@ -13,11 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      
-      return true
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // 네비게이션 뒤로가기 버튼 커스텀
+    if let navigationController = UIApplication.shared.windows.first!.rootViewController as? UINavigationController {
+      let backButtonImage = UIImage(named: "icBack")
+      navigationController.navigationBar.backIndicatorImage = backButtonImage
+      navigationController.navigationBar.backIndicatorTransitionMaskImage = backButtonImage
+      navigationController.navigationBar.tintColor = .smBarButtonItemTint
+      UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear], for: .normal)
+    }
+
+    return true
   }
 }
