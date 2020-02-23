adef90d1db5a09f1303cd42c18cdbace50dededd
Sung Kyungmo <skm280@naver.com>
2020-02-23 23:45:41 +0900

    Realm 유닛 테스트 추가

--- SimpleMemoTests/SimpleMemoTests.swift
+++ SimpleMemoTests/SimpleMemoTests.swift
@@ -7,28 +7,79 @@
 //
 
 import XCTest
+import RealmSwift
+import Nuke
+
 @testable import SimpleMemo
 
 class SimpleMemoTests: XCTestCase {
+  var realm: Realm!
+  var memo: Memo!
+  
+  override func setUp() {
+    super.setUp()
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    realm = try! Realm(
+      configuration: Realm.Configuration(inMemoryIdentifier: self.name)
+    )
+    
+    let urlImage = AttachedImage()
+    urlImage.url = "https://kyungmosung.github.io/img/avatar.jpg"
+    let dataImage = AttachedImage()
+    dataImage.data = UIImage(named: "imagePlaceholder")?.pngData()
+    memo = Memo(title: "title", content: "content", images: [urlImage, dataImage])
+    
+    try! realm.write {
+      realm.add(memo)
     }
+  }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+  override func tearDown() {
+    super.tearDown()
+
+    try! realm.write {
+      realm.deleteAll()
     }
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  /// DB저장 테스트
+  func testSaveMemo() {
+    let memos = realm.objects(Memo.self)
+    XCTAssertEqual(memos.filter { $0.ID == self.memo.ID }.count, 1)
+  }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+  /// 메모 이미지 타입별 테스트
+  func testMemoAttachImageType() {
+    let memos = realm.objects(Memo.self)
+    XCTAssertGreaterThan(memos.count, 0)
+    
+    for memo in memos {
+      for image in memo.images {
+        switch image.type {
+        case .data:
+          XCTAssertNotNil(image.data)
+          XCTAssertNil(image.url)
+
+          // 이미지 데이터 로딩
+          XCTAssertNotNil(UIImage(data: image.data!))
+        case .url:
+          XCTAssertNotNil(image.url)
+          XCTAssertNil(image.data)
+
+          let imageURL = URL(string: image.url!)
+          XCTAssertNotNil(imageURL)
+
+          // 이미지 URL 로딩
+          ImagePipeline.shared.loadImage(with: imageURL!) { result in
+            switch result {
+            case .success(let response):
+              XCTAssertNotNil(response.image)
+            case .failure(let error):
+              XCTFail(error.localizedDescription)
+            }
+          }
         }
+      }
     }
-
+  }
 }
